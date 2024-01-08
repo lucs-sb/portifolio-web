7433ec438032d80eed92468cf69a1c29bf78d35a
Lucas Soares <soares0lucas8@gmail.com>
2024-01-07 22:03:16 -0300

    feat: componente main-animation

---
+++ src/app/shared/main-animation/main-animation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'main-animation',
+  templateUrl: './main-animation.component.html',
+  styleUrls: ['./main-animation.component.css']
+})
+export class MainAnimationComponent {
+
+}
