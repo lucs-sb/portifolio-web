ced019857ac328846aae8bf840b012a6f9697cfc
Lucas Soares <soares0lucas8@gmail.com>
2024-01-07 21:12:27 -0300

    feat: componente btn-primary

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { BtnPrimaryComponent } from './shared/btn-primary/btn-primary.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    BtnPrimaryComponent
   ],
   imports: [
     BrowserModule,
